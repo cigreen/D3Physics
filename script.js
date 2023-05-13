70448436d8436b58bba2a6175a95493e6ab69d6d
cigreen <cigreen@ucsc.edu>
2023-05-12 20:28:42 -0700

    created level two, working on level 3

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ class Intro extends Phaser.Scene {
         let screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
         let screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
         this.add.text(screenCenterX, screenCenterY, 'Click to begin.').setOrigin(0.5);
-        /*this.input.on('pointerdown', () => {
+        this.input.on('pointerdown', () => {
             this.cameras.main.fadeOut(1000, 0,0,0);
-            this.time.delayedCall(1000, () => this.scene.start('levelone'));
-        });*/
+            this.time.delayedCall(1000, () => this.scene.start('leveltwo'));
+        });
     }
 }
 
@@ -34,11 +34,11 @@ class LevelOne extends Phaser.Scene {
         this.load.image("ground", "ground.png");
     }
     create() {
-        const beer = this.physics.add.staticImage(400, 450, 'beer');
+        const beer = this.physics.add.image(800, 500, 'beer').setScale(0.2);
         this.larry = this.physics.add.sprite(100, 350, 'larry').setScale(0.5);
+       
         // after 10 seconds, go to intro SWITCH THAT THIS WORKS POG
         this.time.delayedCall(10000, () => this.scene.start('intro'));
-        //this.larry.setBounce(0.2);
         this.platforms = this.physics.add.staticGroup();
         this.platforms.create(400, 600, 'ground').setScale(2).refreshBody();
 
@@ -47,9 +47,8 @@ class LevelOne extends Phaser.Scene {
         this.balls = this.physics.add.group({
             key: 'ball',
             repeat: 6,
-            setXY: { x: 300, y: 0, stepX: 150 },
+            setXY: { x: 300, y: 525, stepX: 150 },
             bounceX: 1,
-            //bounceY: 1,
             collideWorldBounds: true,
             velocityX: -100
             
@@ -63,7 +62,7 @@ class LevelOne extends Phaser.Scene {
 
         this.physics.add.collider(this.larry, this.platforms);
         this.physics.add.collider(this.platforms, this.balls);
-        // set the below code to a constant and add the touchBall code to it. 
+        this.physics.add.collider(this.platforms, beer);
         const touchBall = this.physics.add.overlap(this.larry, this.balls, (larry, ball) =>
         {
             ball.disableBody(true, true);
@@ -82,13 +81,196 @@ class LevelOne extends Phaser.Scene {
             });
         });
     }
-/* this is crashing the game with beer
-    touchBall (larry, ball)
+
+    update ()
     {
-        ball.disableBody(true, true);
-        this.scene.start('levelone')
+        const { left, right, up } = this.cursors;
+
+        if (left.isDown)
+        {
+            this.larry.setVelocityX(-160);    
+        }
+        else if (right.isDown)
+        {
+            this.larry.setVelocityX(160);   
+        }
+        else
+        {
+            this.larry.setVelocityX(0);  
+        }
+
+        if (up.isDown && this.larry.body.touching.down)
+        {
+            this.larry.setVelocityY(-330);
+        }
+        
+        
     }
-*/
+}
+
+class LevelTwo extends Phaser.Scene {
+    constructor() {
+        super('leveltwo');
+    }
+    preload() {
+        this.load.path = "./assets/";
+        this.load.image("beer", "beer.png");
+        this.load.image("ball", "soccer.png");
+        this.load.image("larry", "dinosaur.png");
+        this.load.image("ground", "ground.png");
+    }
+    // move to intro after getting your collision problem fixed.
+    create() {
+        const beer = this.physics.add.image(800, 500, 'beer').setScale(0.2);
+        this.larry = this.physics.add.sprite(100, 350, 'larry').setScale(0.5);
+       
+        // after 10 seconds, go to intro SWITCH THAT THIS WORKS POG
+        this.time.delayedCall(10000, () => this.scene.start('intro'));
+        this.platforms = this.physics.add.staticGroup();
+        this.platforms.create(400, 600, 'ground').setScale(2).refreshBody();
+
+        this.movingPlatform = this.physics.add.image(600, 500, 'ground').setScale(0.5);
+
+        this.movingPlatform.setImmovable(true);
+        this.movingPlatform.body.allowGravity = false;
+        this.movingPlatform.setVelocityX(50);
+
+        this.larry.setCollideWorldBounds(true);
+
+        this.balls = this.physics.add.group({
+            key: 'ball',
+            repeat: 6,
+            setXY: { x: 300, y: 325, stepX: 150 },
+            bounceX: 1,
+            collideWorldBounds: true,
+            velocityX: -100
+            
+        });
+        for (const ball of this.balls.getChildren())
+        {
+            ball.setBounceY(Phaser.Math.FloatBetween(0.4, 0.5));
+            ball.setScale(0.1);
+        }
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.physics.add.collider(this.larry, this.platforms);
+        this.physics.add.collider(this.platforms, this.balls);
+        this.physics.add.collider(this.platforms, beer);
+        this.physics.add.collider(this.larry, this.movingPlatform);
+        this.physics.add.collider(this.balls, this.movingPlatform);
+        const touchBall = this.physics.add.overlap(this.larry, this.balls, (larry, ball) =>
+        {
+            ball.disableBody(true, true);
+            this.scene.start('leveltwo');
+        });
+        // when colliding with beer, become invulnerable
+        this.physics.add.overlap(this.larry, beer, (larry, _beer) =>
+        {
+            touchBall.active = false;
+            larry.setTintFill(0xffff00);
+            _beer.destroy();
+            this.time.delayedCall(5000, () =>
+            {
+                touchBall.active = true;
+                larry.clearTint();
+            });
+        });
+    }
+
+    update ()
+    {
+        const { left, right, up } = this.cursors;
+
+        if (left.isDown)
+        {
+            this.larry.setVelocityX(-160);    
+        }
+        else if (right.isDown)
+        {
+            this.larry.setVelocityX(160);   
+        }
+        else
+        {
+            this.larry.setVelocityX(0);  
+        }
+
+        if (up.isDown && this.larry.body.touching.down)
+        {
+            this.larry.setVelocityY(-330);
+        }
+        
+        if (this.movingPlatform.x >= 600)
+        {
+            this.movingPlatform.setVelocityX(-50);
+        }
+        else if (this.movingPlatform.x <= 400)
+        {
+            this.movingPlatform.setVelocityX(50);
+        }
+    }
+}
+
+class LevelThree extends Phaser.Scene {
+    constructor() {
+        super('levelthree');
+    }
+    // move to intro after getting your collision problem fixed.
+    preload() {
+        this.load.path = "./assets/";
+        this.load.image("beer", "beer.png");
+        this.load.image("ball", "soccer.png");
+        this.load.image("larry", "dinosaur.png");
+        this.load.image("ground", "ground.png");
+    }
+    create() {
+        const beer = this.physics.add.image(800, 500, 'beer').setScale(0.2);
+        this.larry = this.physics.add.sprite(100, 350, 'larry').setScale(0.5);
+       
+        // after 10 seconds, go to intro SWITCH THAT THIS WORKS POG
+        this.time.delayedCall(10000, () => this.scene.start('intro'));
+        this.platforms = this.physics.add.staticGroup();
+        this.platforms.create(400, 600, 'ground').setScale(2).refreshBody();
+
+        this.larry.setCollideWorldBounds(true);
+
+        this.balls = this.physics.add.group({
+            key: 'ball',
+            repeat: 6,
+            setXY: { x: 300, y: 525, stepX: 150 },
+            bounceX: 1,
+            collideWorldBounds: true,
+            velocityX: -100
+            
+        });
+        for (const ball of this.balls.getChildren())
+        {
+            ball.setBounceY(Phaser.Math.FloatBetween(0.4, 0.5));
+            ball.setScale(0.1);
+        }
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.physics.add.collider(this.larry, this.platforms);
+        this.physics.add.collider(this.platforms, this.balls);
+        this.physics.add.collider(this.platforms, beer);
+        const touchBall = this.physics.add.overlap(this.larry, this.balls, (larry, ball) =>
+        {
+            ball.disableBody(true, true);
+            this.scene.start('levelone');
+        });
+        // when colliding with beer, become invulnerable
+        this.physics.add.overlap(this.larry, beer, (larry, _beer) =>
+        {
+            touchBall.active = false;
+            larry.setTintFill(0xffff00);
+            _beer.destroy();
+            this.time.delayedCall(5000, () =>
+            {
+                touchBall.active = true;
+                larry.clearTint();
+            });
+        });
+    }
+
     update ()
     {
         const { left, right, up } = this.cursors;
@@ -126,5 +308,5 @@ new Phaser.Game({
             debug: true
         }
     },
-    scene: [LevelOne, Intro],
+    scene: [LevelThree, LevelTwo, Intro, LevelOne],
 });
